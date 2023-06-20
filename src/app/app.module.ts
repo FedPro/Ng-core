8147bbd2f46eb3473466e3c6904c6f98447d69ee
diprofe <Federica.DIPROPERZIO@netcompany-intrasoft.com>
2023-06-20 21:00:18 +0200

    FilterByCategoryPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { FilterByCategoryPipe } from './filter-by-category.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseCardComponent
+    CourseCardComponent,
+    FilterByCategoryPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [FilterByCategoryPipe],
   providers: [],
   bootstrap: [AppComponent]
 })
