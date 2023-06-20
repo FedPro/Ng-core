3a6ac52d6c5200b1573b54d0fa0149f05160426e
diprofe <Federica.DIPROPERZIO@netcompany-intrasoft.com>
2023-06-20 17:02:48 +0200

    Input and Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { COURSES } from 'src/db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ng-core';
+  courses = COURSES;
+
+  onCourseSelected(course: Course){
+    console.log("onCardClicked [APP]", course);
+  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'course-card',
   templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+  styleUrls: ['./course-card.component.css'],
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements OnInit {
+  constructor() {}
 
+  @Input({ required: true })
+  course!: Course;
+
+  @Output('courseSelected')
+  courseEmitter= new EventEmitter<Course>();
+
+  ngOnInit(): void {}
+
+  onCourseViewed(){
+    console.log('CLICK component LVL')
+    this.courseEmitter.emit(this.course)
+  }
 }
